7fea496bcb2b8f750fd20134199cd206fb8fd4d1
Edouard Hinard <edouard.hinard@orange.com>
2017-07-19 13:45:40 +0200

    choix framework

---
+++ el.py
@@ -0,0 +1,57 @@
+#! /usr/bin/env python3
+# coding: utf-8
+
+import sys
+from PyQt5.QtWidgets import QWidget, QApplication, QDesktopWidget
+from PyQt5.QtGui import QPainter, QColor, QFont, QIcon
+from PyQt5.QtCore import Qt
+
+
+class Example(QWidget):
+    
+    def __init__(self):
+        super().__init__()
+        
+        self.initUI()
+        
+        
+    def initUI(self):      
+
+        self.text = u'\u041b\u0435\u0432 \u041d\u0438\u043a\u043e\u043b\u0430\
+\u0435\u0432\u0438\u0447 \u0422\u043e\u043b\u0441\u0442\u043e\u0439: \n\
+\u0410\u043d\u043d\u0430 \u041a\u0430\u0440\u0435\u043d\u0438\u043d\u0430'
+
+        #self.setGeometry(300, 300, 280, 170)
+        self.resize(250, 150)
+        self.center()
+        self.setWindowTitle('Draw text')
+        self.setWindowIcon(QIcon('web.png')) 
+        self.show()
+        
+    def center(self):
+        
+        qr = self.frameGeometry()
+        cp = QDesktopWidget().availableGeometry().center()
+        qr.moveCenter(cp)
+        self.move(qr.topLeft())
+        
+    def paintEvent(self, event):
+
+        qp = QPainter()
+        qp.begin(self)
+        self.drawText(event, qp)
+        qp.end()
+        
+        
+    def drawText(self, event, qp):
+      
+        qp.setPen(QColor(168, 34, 3))
+        qp.setFont(QFont('Decorative', 10))
+        qp.drawText(event.rect(), Qt.AlignCenter, self.text)        
+                
+        
+if __name__ == '__main__':
+    
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
