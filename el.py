2a20c4f5f81d375323b8cbc1056b6687fddb9692
Edouard Hinard <edouard.hinard@orange.com>
2017-07-20 08:00:08 +0200

    1ère fenêtre

--- el.py
+++ el.py
@@ -1,43 +1,46 @@
 #! /usr/bin/env python3
 # coding: utf-8
 
+#designer -qt=4 el.ui
+#pyuic5 el.ui -x -o el-gui.py
+
+
 import sys
-from PyQt5.QtWidgets import QWidget, QApplication, QDesktopWidget
-from PyQt5.QtGui import QPainter, QColor, QFont, QIcon
-from PyQt5.QtCore import Qt
+from PyQt5 import QtCore, QtGui, QtWidgets
+from el_gui import Ui_MainWindow
 
 
-class Example(QWidget):
-    
-    def __init__(self):
-        super().__init__()
-        
-        self.initUI()
-        
-        
-    def initUI(self):      
 
-        self.text = u'\u041b\u0435\u0432 \u041d\u0438\u043a\u043e\u043b\u0430\
-\u0435\u0432\u0438\u0447 \u0422\u043e\u043b\u0441\u0442\u043e\u0439: \n\
-\u0410\u043d\u043d\u0430 \u041a\u0430\u0440\u0435\u043d\u0438\u043d\u0430'
-
-        #self.setGeometry(300, 300, 280, 170)
-        self.resize(250, 150)
-        self.center()
-        self.setWindowTitle('Draw text')
-        self.setWindowIcon(QIcon('web.png')) 
-        self.show()
-        
-    def center(self):
-        
-        qr = self.frameGeometry()
-        cp = QDesktopWidget().availableGeometry().center()
-        qr.moveCenter(cp)
-        self.move(qr.topLeft())
+class QMyFrame(QtWidgets.QFrame):
+#    def __init__(self):
+#        super().__init__()
+#        
+#        self.initUI()
+#        
+#        
+#    def initUI(self):      
+#
+#        self.text = u'\u041b\u0435\u0432 \u041d\u0438\u043a\u043e\u043b\u0430\
+#\u0435\u0432\u0438\u0447 \u0422\u043e\u043b\u0441\u0442\u043e\u0439: \n\
+#\u0410\u043d\u043d\u0430 \u041a\u0430\u0440\u0435\u043d\u0438\u043d\u0430'
+#
+#        #self.setGeometry(300, 300, 280, 170)
+#        self.resize(250, 150)
+#        self.center()
+#        self.setWindowTitle('Draw text')
+#        self.setWindowIcon(QIcon('web.png')) 
+#        self.show()
+#        
+#    def center(self):
+#        
+#        qr = self.frameGeometry()
+#        cp = QDesktopWidget().availableGeometry().center()
+#        qr.moveCenter(cp)
+#        self.move(qr.topLeft())
         
     def paintEvent(self, event):
 
-        qp = QPainter()
+        qp = QtGui.QPainter()
         qp.begin(self)
         self.drawText(event, qp)
         qp.end()
@@ -45,13 +48,18 @@ class Example(QWidget):
         
     def drawText(self, event, qp):
       
-        qp.setPen(QColor(168, 34, 3))
-        qp.setFont(QFont('Decorative', 10))
-        qp.drawText(event.rect(), Qt.AlignCenter, self.text)        
+        qp.setPen(QtGui.QColor(168, 34, 3))
+        qp.setFont(QtGui.QFont('Decorative', 10))
+        qp.drawText(event.rect(), QtCore.Qt.AlignCenter, "bébé")
                 
-        
-if __name__ == '__main__':
-    
-    app = QApplication(sys.argv)
-    ex = Example()
+
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
     sys.exit(app.exec_())
+
---
+++ el_gui.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'el.ui'
+#
+# Created by: PyQt5 UI code generator 5.9
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(602, 499)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.verticalLayout = QtWidgets.QVBoxLayout(self.centralwidget)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.frame = QMyFrame(self.centralwidget)
+        self.frame.setFrameShape(QtWidgets.QFrame.StyledPanel)
+        self.frame.setFrameShadow(QtWidgets.QFrame.Raised)
+        self.frame.setObjectName("frame")
+        self.verticalLayout.addWidget(self.frame)
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setObjectName("pushButton")
+        self.verticalLayout.addWidget(self.pushButton)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 602, 27))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.pushButton.setText(_translate("MainWindow", "Recharger"))
+
+from el import QMyFrame
